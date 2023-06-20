dc135d3f9bcdb90f5721432caace423a3eee40a4
maxidragon <mgwspinanie2006@wp.pl>
2023-06-20 14:53:20 +0200

    Add prisma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: [
+      'Authorization',
+      'X-Requested-With',
+      'Content-Type',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Credentials',
+      'Origin',
+    ],
+  });
   await app.listen(3000);
 }
+
 bootstrap();
