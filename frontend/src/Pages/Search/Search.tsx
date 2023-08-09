e20e651465e5b09cc34ba8fce885e35e4d1046e5
maxidragon <mgwspinanie2006@wp.pl>
2023-08-09 18:20:04 +0200

    Code cleanup

--- frontend/src/Pages/Search/Search.tsx
+++ frontend/src/Pages/Search/Search.tsx
@@ -1,17 +1,4 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  Chip,
-  FormControl,
-  FormControlLabel,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Checkbox, FormControlLabel, TextField, Typography } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import {
   getAllCountries,
@@ -22,6 +9,10 @@ import { calculateTotalPages, formatSearchQuery } from "../../logic/other";
 import { searchUsers } from "../../logic/user";
 import SearchResult from "./SearchResult/SearchResult";
 import { Country, Hobby, Language, User } from "../../logic/interfaces";
+import GenderSelect from "../../Components/Search/GenderSelect";
+import LanguageSelect from "../../Components/Search/LanguageSelect";
+import CountrySelect from "../../Components/Search/CountrySelect";
+import HobbySelect from "../../Components/Search/HobbySelect";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -151,143 +142,10 @@ const Search = () => {
             variant="outlined"
             inputRef={usernameRef}
           />
-          <FormControl sx={{ mt: 2, width: "100%" }}>
-            <InputLabel id="hobbies-label">Hobbies</InputLabel>
-            <Select
-              labelId="hobbies-label"
-              multiple
-              value={selectedHobbies}
-              onChange={handleHobbySelectChange}
-              input={<OutlinedInput label="Chip" />}
-              renderValue={(selected: number[]) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value: number) => {
-                    const hobby = possibleHobbies.find(
-                      (h: Hobby) => h.id === value
-                    );
-                    return <Chip key={value} label={hobby ? hobby.name : ""} />;
-                  })}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {possibleHobbies.map((hobby: { id: number; name: string }) => (
-                <MenuItem
-                  key={hobby.id}
-                  value={hobby.id}
-                >
-                  {hobby.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ mt: 2, width: "100%" }}>
-            <InputLabel id="countries-label">Countries</InputLabel>
-            <Select
-              labelId="countries-label"
-              multiple
-              value={selectedCountries}
-              onChange={handleCountrySelectChange}
-              input={<OutlinedInput label="Chip" />}
-              renderValue={(selected: number[]) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value: number) => {
-                    const country = possibleCountries.find(
-                      (c: Country) => c.id === value
-                    );
-                    return (
-                      <Chip key={value} label={country ? country.name : ""} />
-                    );
-                  })}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {possibleCountries.map(
-                (country: Country) => (
-                  <MenuItem
-                    key={country.id}
-                    value={country.id}
-                  >
-                    {country.name}
-                  </MenuItem>
-                )
-              )}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ mt: 2, width: "100%" }}>
-            <InputLabel id="languages-label">Languages</InputLabel>
-            <Select
-              labelId="languages-label"
-              multiple
-              value={selectedLanguages}
-              onChange={handleLanguageSelectChange}
-              input={<OutlinedInput label="Chip" />}
-              renderValue={(selected: number[]) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value: number) => {
-                    const language = possibleLanguages.find(
-                      (c: Language) => c.id === value
-                    );
-                    return (
-                      <Chip key={value} label={language ? language.name : ""} />
-                    );
-                  })}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {possibleLanguages.map(
-                (language: { id: number; name: string }) => (
-                  <MenuItem
-                    key={language.id}
-                    value={language.id}
-                  >
-                    {language.name}
-                  </MenuItem>
-                )
-              )}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ mt: 2, width: "100%" }}>
-            <InputLabel id="gender-label">Gender</InputLabel>
-            <Select
-              labelId="gender-label"
-              multiple
-              value={selectedGender}
-              onChange={handleGenderChange}
-              input={<OutlinedInput label="Chip" />}
-              renderValue={(selected: string[]) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value: string) => {
-                    return (
-                      <Chip key={value} label={value === "MALE" ? "Male" : value === "FEMALE" ? "Female" : "Other"} />
-                    );
-                  })}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              <MenuItem
-                key={"FEMALE"}
-                value={"FEMALE"}
-              >
-                Female
-              </MenuItem>
-              <MenuItem
-                key={"MALE"}
-                value={"MALE"}
-              >
-                Male
-              </MenuItem>
-              <MenuItem
-                key={"OTHER"}
-                value={"OTHER"}
-              >
-                Other
-              </MenuItem>
-            </Select>
-          </FormControl>
+          <HobbySelect selectedHobbies={selectedHobbies} handleHobbySelectChange={handleHobbySelectChange} menu={MenuProps} possibleHobbies={possibleHobbies} />
+          <CountrySelect selectedCountries={selectedCountries} handleCountrySelectChange={handleCountrySelectChange} menu={MenuProps} possibleCountries={possibleCountries} />
+          <LanguageSelect selectedLanguages={selectedLanguages} handleLanguageSelectChange={handleLanguageSelectChange} menu={MenuProps} possibleLanguages={possibleLanguages} />
+          <GenderSelect selectedGender={selectedGender} handleGenderChange={handleGenderChange} menu={MenuProps} />
           <FormControlLabel control={<Checkbox onChange={handleOnlyWithDescriptionChange} checked={onlyWithDescription} />} label="Only with description" />
           <Button
             variant="contained"
